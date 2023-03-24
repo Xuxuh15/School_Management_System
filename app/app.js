a39c71b5c3e1d8de02cc18e3ae9de98f6445f9c2
Xuxuh <jk.hilton@hotmail.com>
2023-03-24 22:30:03 +0100

    First commit

---
+++ app/app.js
@@ -0,0 +1,7 @@
+//uses core node module
+const express = require('express'); 
+const morgan = require('morgan');
+const app = express();   //assign express to app variable
+
+module.exports = app; 
+
