3a2d66abeb7d187641faae04029ae4a87c7f4d6a
Xuxuh <jk.hilton@hotmail.com>
2023-04-02 23:04:12 +0200

    Moved logic to controller file.

--- routes/Staff/adminRouter.js
+++ routes/Staff/adminRouter.js
@@ -1,44 +1,81 @@
 const express = require('express'); 
-
 const adminRouter = express.Router(); 
+const {adminRegisterCtrl,adminLoginCtrl,adminGetAllCtrl,
+adminGetSingleCtrl,adminUpdateAdminCtrl,adminDeleteAdminCtrl,
+adminSuspendTeacherCtrl,adminUnsuspendTeacherCtrl,
+adminWithdrawlTeacherCtrl,adminUnwithdrawlTeacherCtrl,
+adminPublishExamCtrl,adminUnpublishExamCtrl} = require('../../controller/Staff/adminCtrl');
 
-//admin register
+//@desc admin register
+//@route POST /api/v1/admins/register
+//@access private
 
-adminRouter.post('/register',(req, res)=>{
-    try{
-        res.status(201).json({
-            status: "success",
-            data: "Admin has been registered",
-        }); 
-    }
-    catch(error){
-        res.json({
-            status: "failed",
-            error: error.message, 
-        });  
-    }
-});
+adminRouter.post('/register', adminRegisterCtrl);
 
-//admin login
+//@desc admin login
+//@route  POST /api/v1/admins/login
+//@access private
 
-adminRouter.post('/',(req, res)=>{
-    try{
-        res.status(201).json({
-            status: "success",
-            data: "Admin has been logged in",
-        }); 
-    }
-    catch(error){
-        res.json({
-            status: "failed",
-            error: error.message, 
-        });  
-    }
-});
+adminRouter.post('/login', adminLoginCtrl);
 
-//admin get all
+//@desc admin get all
+//@route GET /api/v1/admins
+//@access private
 
-//admin get single
+adminRouter.get('/', adminGetAllCtrl);
+
+//@desc admin get single admin
+//@route GET /api/v1/admins/:id
+//@access private
+
+adminRouter.get('/:id', adminGetSingleCtrl);
+
+
+//@desc admin update admin
+//@route PUT /api/v1/admins/:id
+//@access private
+adminRouter.put('/:id', adminUpdateAdminCtrl )
+
+//@desc admin delete admin
+//@route DELETE /api/v1/admins/:id
+//@access private
+adminRouter.delete('/:id', adminDeleteAdminCtrl );
+
+//@desc admin suspend teacher
+//@route PUT /api/v1/admins/suspend/teacher/:id
+//@access private
+adminRouter.put('/suspend/teacher/:id', adminSuspendTeacherCtrl);
+
+//@desc admin unsuspend teacher
+//@route GET /api/v1/admins/unsuspend/teacher/:id
+//@access private
+adminRouter.put('/unsuspend/teacher/:id',adminUnsuspendTeacherCtrl );
+
+//@desc admin withdrawl teacher
+//@route PUT /api/v1/admins/withdrawl/teacher/:id
+//@access private
+
+adminRouter.put('/withdrawl/teacher/:id', adminUnwithdrawlTeacherCtrl );
+
+//@desc admin unwithdrawl teacher
+//@route GET /api/v1/admins/unwithdrawl/teacher/:id
+//@access private
+
+adminRouter.put('/unwithdrawl/teacher/:id', adminUnwithdrawlTeacherCtrl );
+
+//@desc admin publish exam
+//@route GET /api/v1/admins/publish/exam/:id
+//@access private
+
+adminRouter.put('/publish/exam/:id', adminPublishExamCtrl);
+
+//@desc admin unpublish exam
+//@route GET /api/v1/admins/unpublish/exam/:id
+//@access private
+
+adminRouter.put('/unpublish/exam/:id',adminUnpublishExamCtrl);  
+
+
 
 module.exports = adminRouter; 
 
