d4f40cd0fae30ee7a27404c5e0f11feefdba61c6
Xuxuh <jk.hilton@hotmail.com>
2023-04-03 13:44:23 +0200

    Added middleware to hash password before document is added to database

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 //initialies a new mongoose Scheme (structure for documents that go in our collection)
 const adminSchema = new mongoose.Schema(
@@ -25,6 +26,20 @@ const adminSchema = new mongoose.Schema(
     }
 ); 
 
+//premiddleware before saving document into database
+//uses function syntax as opposed to arrow to allow for bidning of this (instance of admin)
+
+adminSchema.pre('save', async function(next){
+    console.log('I have been called');
+    //to avoid rehashing password when admin is updated
+    if(!this.isModified('password')){
+        next();
+    }
+    //salt
+    const salt = await bcrypt.genSalt(10); 
+    this.password = await bcrypt.hash(this.password,salt);
+    next(); //instance of admin password
+});
 
 //model
 
