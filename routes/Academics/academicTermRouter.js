04f122db13c103324c5e884c4ef193435230c142
Xuxuh <jk.hilton@hotmail.com>
2023-04-10 12:02:48 +0200

    Added routes for academic term

--- routes/Academics/academicTermRouter.js
+++ routes/Academics/academicTermRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const isLoggedIn = require('../../middleware/isLoggedIn');
 const isAdmin = require('../../middleware/isAdmin');
-const {createAcademicTerm, getAcademicTerms} = require('../../controller/Academics/academicTermCtrl');
+const {createAcademicTerm, getAcademicTerms, getAcademicTerm, updateAcademicTerm, deleteAcademicTerm} = require('../../controller/Academics/academicTermCtrl');
 
 
 const academicTermRouter = express.Router();
@@ -12,6 +12,16 @@ academicTermRouter
 .post(isLoggedIn, isAdmin, createAcademicTerm)
 //fetch all academic terms
 .get(isLoggedIn, isAdmin, getAcademicTerms)
+
+academicTermRouter
+.route('/:id')
+//get single academic term
+.get(isLoggedIn, isAdmin, getAcademicTerm)
+//update single academic term
+.put(isLoggedIn, isAdmin, updateAcademicTerm)
+//delete a single academic term
+.delete(isLoggedIn, isAdmin, deleteAcademicTerm)
+
 
 
 module.exports = academicTermRouter;
