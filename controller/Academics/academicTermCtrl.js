451c05bb69876ba1c6a0478a792754a65d0092ea
Xuxuh <jk.hilton@hotmail.com>
2023-04-10 12:00:59 +0200

    Logic for CRUD operations for Academic Term

--- controller/Academics/academicTermCtrl.js
+++ controller/Academics/academicTermCtrl.js
@@ -50,3 +50,79 @@ exports.getAcademicTerms = AsyncHandler(async(req, res)=>{
     });
 });
 
+//@desc Fetches a single academic term from database
+// GET /api/v1/academic-terms/:id
+//@private
+exports.getAcademicTerm = AsyncHandler(async(req, res)=>{
+    const academicTermFound = await AcademicTerm.findById(req.params.id);
+
+    if(academicTermFound){
+        res.status(200).json({
+            status: "Success",
+            message: "Academic term successfully retrieved",
+            data: academicTermFound
+        });
+    }
+    else{
+        throw new Error('Academic term could not be found');
+    }
+
+});
+
+//@desc Updates a single academic term from database
+// PUT /api/v1/academic-terms/:id
+//@private
+exports.updateAcademicTerm = AsyncHandler(async(req, res)=>{
+    const {name, description, duration} = req.body; 
+
+    const academicTermFound = await AcademicTerm.findOne({name});
+
+    if(academicTermFound){
+        throw new Error('Academic term already exists');
+    }
+
+    const academicTermToUpdate = await AcademicTerm.findByIdAndUpdate(req.params.id,
+        {
+            name,
+            description,
+            duration,
+            createdBy: req.userAuth._id
+        },
+        {
+            new: true
+        }
+        ); 
+        res.status(201).json({
+            status: "Success",
+            message: "Academic term is successfully updated",
+            data: academicTermToUpdate
+        }); 
+
+});
+
+//@desc Deletes a single academic term from database
+// DELETE /api/v1/academic-terms/:id
+//@private
+exports.deleteAcademicTerm = AsyncHandler(async(req, res)=>{
+    //check if academic term exists
+    const academicTermToDelete = await AcademicTerm.findById(req.params.id);
+    if(!academicTermToDelete){
+        throw new Error('Academic term does not exist');
+    }
+    //delete academic term
+    await AcademicTerm.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        status: "Success",
+        message: "Academic term deleted successfully"
+    });
+});
+
+
+
+
+
+
+
+
+
