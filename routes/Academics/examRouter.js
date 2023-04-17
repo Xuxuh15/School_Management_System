e6b38167eeadda02645277ff6a71741d56fa15e5
Xuxuh <jk.hilton@hotmail.com>
2023-04-17 19:58:27 +0200

    Updated exam routes

--- routes/Academics/examRouter.js
+++ routes/Academics/examRouter.js
@@ -5,10 +5,19 @@ const isLoggedInTeacher = require('../../middleware/isLoggedInTeacher');
 const isTeacher = require('../../middleware/isTeacher'); 
 //middleware checks if user is an Admin
 const isAdmin = require('../../middleware/isAdmin'); 
+const {createExam, getExams} = require('../../controller/Academics/examCtrl');
 
 
 //create new exam router
 const examRouter = express.Router();
+
+//teacher create exam
+examRouter.post('/', isLoggedInTeacher, isTeacher, createExam); 
+
+//fetch all exams
+examRouter.get('/', isLoggedInTeacher, isTeacher, getExams);
+
+
 
 
 
